b07505098744d0becccd9f1c4e1c52b6857d47e2
minho <kmn400@nate.com>
2021-12-22 01:24:41 +0900

    211222_영화검색과제

--- lib/data/movie_api.dart
+++ lib/data/movie_api.dart
@@ -12,7 +12,7 @@ class MovieApi {
       List jsonList = jsonDecode(response.body)['results'];
       return jsonList.map((e) => Movie.fromJson(e)).toList();
     } else {
-      throw Exception('Faile d to load album');
+      throw Exception('Failed to load album');
     }
   }
 }
