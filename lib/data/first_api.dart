a05be3ff357cca5ed6dca9000e60fe06670e17c6
minho <kmn400@nate.com>
2022-01-10 01:10:22 +0900

    220109_초기화면설정_'first_api'만듦

--- lib/data/first_api.dart
+++ lib/data/first_api.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'package:exam2_movie/model/movie1.dart';
 import 'package:http/http.dart' as http;
 
-class MovieApi {
-  Future<List<Results>> fetchMovies(String query1) async {
+class FirstApi {
+  Future<List<Results>> firstMovies(String query1) async {
     final response = await http.get(Uri.parse(
-        'https://api.themoviedb.org/3/search/movie?api_key=a64533e7ece6c72731da47c9c8bc691f&query=$query1&language=en-EN'));
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
 
     if (response.statusCode == 200) {
       List jsonList = jsonDecode(response.body)['results'];
--- lib/ui/search_screen.dart
+++ lib/ui/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:exam2_movie/data/first_api.dart';
 import 'package:exam2_movie/data/movie_api.dart';
 import 'package:exam2_movie/model/movie1.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +14,21 @@ class _SearchScreenState extends State<SearchScreen> {
   List<Results> _movies = [];
 
   final _api = MovieApi();
+  final _firstApi = FirstApi();
   final _textEditingController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _showResult('spider');
+
+    Future<void> _show(String query) async {
+      List<Results> movies = await _firstApi.firstMovies(query);
+      setState(() {
+        _movies = movies;
+      });
+    }
+
+    _show('');
   }
 
   @override
@@ -38,7 +48,7 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('영화  정보 검색기'),
+        title: const Text('영화 정보 검색기'),
       ),
       body: Column(
         children: [
@@ -54,12 +64,19 @@ class _SearchScreenState extends State<SearchScreen> {
             ),
           ),
           Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 12,
-              crossAxisSpacing: 0,
-              children:
-                  _movies.map((e) => Image.network(e.posterPath)).toList(),
+            child: Column(
+              children: [
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 1,
+                    children: _movies
+                        .map((e) => Image.network(e.posterPath))
+                        .toList(),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
